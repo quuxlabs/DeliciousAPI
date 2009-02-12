2f9e90d23da09fa082467459b7b1192aec6e2bb8
Michael G. Noll <coding@michael-noll.com>
2009-02-12 19:00:55 +0100

    Bumbed version to 1.5.11

--- deliciousapi.py
+++ deliciousapi.py
@@ -49,7 +49,7 @@ __license__ = "GPLv2"
 __maintainer__ = "Michael G. Noll"
 __status__ = "Development"
 __url__ = "http://www.michael-noll.com/"
-__version__ = "1.5.10"
+__version__ = "1.5.11"
 
 import base64
 import cgi
